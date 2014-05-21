9f6313046feec44de5eb0abbce38af7f8f8e4a57
javiermon <javiermon@gmail.com>
2014-05-21 16:12:53 +0200

    fix "find: Argument list too long"

--- clean-thumbs.sh
+++ clean-thumbs.sh
@@ -1,6 +1,6 @@
 #!/bin/ash
 # remove older than 1 month thumbs
 
-find .thumbnails/common/* -mtime +30 | xargs rm -rf
-find .thumbnails/filebox/* -mtime +30 | xargs rm -rf
-find .thumbnails/grid/* -mtime +30 | xargs rm -rf
+find .thumbnails/common/ -type f -mtime +30 | xargs rm -rf
+find .thumbnails/filebox/ -type f -mtime +30 | xargs rm -rf
+find .thumbnails/grid/ -type f -mtime +30 | xargs rm -rf
